6258898b50c403215f475cd5eabfcacdd38e2c05
furkanyigit438 <furkan.yigit.naz.akyol@gmail.com>
2025-12-25 16:56:58 +0300

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -2,7 +2,7 @@ local function farm()
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/gBZXCz7Z0h/0", true))()
 end
 local function loadui()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/furkanyigit438/script/refs/heads/main/mm2.lua"))() 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/MurderMystery2/main.lua", true))() 
 end
 
 task.spawn(farm)
